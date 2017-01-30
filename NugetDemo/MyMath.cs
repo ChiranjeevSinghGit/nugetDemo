17412242fce8d7becfcbe9c37cf4cc2708231c83
NugetDemo <chiranjeev_singh@infosys.com>
2017-01-30 16:41:30 +0530

    Updated NugetDemo

---
+++ NugetDemo/MyMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetDemo
+{
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public decimal Div(int a, int b)
+        {
+            return a / b;
+        }
+       
+    }
+}
---
+++ TestNuget/Program.cs
@@ -0,0 +1,22 @@
+﻿using NugetDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            MyMath math = new MyMath();
+            Console.Write("a+b={0}", math.Add(a, b));
+            Console.ReadKey();
+
+        }
+    }
+}
